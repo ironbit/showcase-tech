74528928ebbe7657b0e70a2866ac28eaaf99a588
Christian Noboa Mardini <ironbit@users.noreply.github.com>
2020-09-28 22:51:21 -0500

    feat: updated the router for the author store request.

--- golang-beego-postgres/app/routers/router.go
+++ golang-beego-postgres/app/routers/router.go
@@ -1,30 +1,15 @@
-// @APIVersion 1.0.0
-// @Title beego Test API
-// @Description beego has a very cool tools to autogenerate documents for your API
-// @Contact astaxie@gmail.com
-// @TermsOfServiceUrl http://beego.me/
-// @License Apache 2.0
-// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
 package routers
 
 import (
-	"sketch-tech/golang-beego-postgres/app/controllers"
-
 	"github.com/astaxie/beego"
+
+	"sketch-tech/golang-beego-postgres/app/controllers"
 )
 
 func init() {
 	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
+		beego.NSRouter("/author", &controllers.AuthorController{}, "post:Store"),
+		//beego.NSRouter("/book/:id", &controllers.BookController{}, "get:GetBookByID"),
 	)
 	beego.AddNamespace(ns)
 }
